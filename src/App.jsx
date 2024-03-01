3d8a99793c417af7798ded5866bdc2bdb25272a0
Pankaj <pankajchadda282@gmail.com>
2024-03-01 18:21:14 +0530

    installed styled components. Create Global theme with styled component. Create CustomRadio. Used Navlink from react-router-dom to style active link

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
+import { GlobalTheme } from './theme';
 // Create a client
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/starred" element={<Starred />} />
-          </Route>
-          <Route path="/show/:showId" element={<Show />} />
-          <Route path="*" element={<div>Not Found</div>} />
-        </Routes>
-      </BrowserRouter>
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
+            <Route path="/show/:showId" element={<Show />} />
+            <Route path="*" element={<div>Not Found</div>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSearchStr } from '../lib/useSearchStr';
+import CustomRadio from './CustomRadio';
 const SearchForm = ({ onSearch }) => {
   const [searchStr, setSearchStr] = useSearchStr();
   const [searchOption, setSearchOption] = useState('shows');
@@ -26,26 +27,20 @@ const SearchForm = ({ onSearch }) => {
     <div>
       <form onSubmit={onSubmit}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
-        <label>
-          Shows
-          <input
-            type="radio"
-            name="search-option"
-            value="shows"
-            checked={searchOption === 'shows'}
-            onChange={onRadioChange}
-          />
-        </label>
-        <label>
-          Actors
-          <input
-            type="radio"
-            name="search-option"
-            value="actors"
-            checked={searchOption === 'actors'}
-            onChange={onRadioChange}
-          />
-        </label>
+        <CustomRadio
+          label="Shows"
+          name="search-option"
+          value="shows"
+          checked={searchOption === 'shows'}
+          onChange={onRadioChange}
+        />
+        <CustomRadio
+          label="Actors"
+          name="search-option"
+          value="actors"
+          checked={searchOption === 'actors'}
+          onChange={onRadioChange}
+        />
         <button type="submit">Search</button>
       </form>
     </div>
